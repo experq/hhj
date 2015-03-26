72752d052ecc44f72f193f8b2655b04e876ebf78
Baiquan Fan <baiquan.fan@experq.com>
2015-03-26 16:16:59 +0200

    Fixes for 'recommendations_controller_spec.rb'.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,7 +4,6 @@
 FactoryGirl.define do
   factory :user do
     email      'halloped@localhost.fi'  # add a fake email for User validation
-    #initialize_with { User.first_or_create }
 
     association :university, :factory => :spartan_teknillinen_yliopisto
 
@@ -16,8 +15,9 @@ FactoryGirl.define do
       phone      '+358 40 123 1234'
       principal_name 'eizit@sty.fi'
       role       :role_union_employee
-      #initialize_with { User.first_or_create }
+      initialize_with { User.first_or_create }
     end
+
     factory :aaro do
       #_id        '4f7963bf91bc2bc1f6000005'
       first_name 'Aaro'
@@ -27,6 +27,7 @@ FactoryGirl.define do
       principal_name 'aaroha@sty.fi'
       role       :role_university_staff
     end
+
     factory :student_martti do
       #_id        '4f7963bf91bc2bc1f6000002'
       first_name 'Martti'
@@ -35,6 +36,7 @@ FactoryGirl.define do
       phone      '+358 40 555 4321'
       principal_name 'marpul@sty.fi'
     end
+
     factory :student_tiina do
       #_id        '4f7963bf91bc2bc1f6000007'
       first_name 'Tiina'
@@ -43,6 +45,7 @@ FactoryGirl.define do
       phone      '+358 40 555 4999'
       principal_name 'tiina@sty.fi'
     end
+
     factory :student_emma do
       #_id        '4f7963bf91bc2bc1f6000003'
       first_name 'Emma'
@@ -50,12 +53,14 @@ FactoryGirl.define do
       email      'emma@spu.fi'
       phone      '+358 40 555 3333'
     end
+
     factory :student_topias do
       #_id        '4f7963bf91bc2bc1f6000004'
       first_name 'Topias'
       last_name  'Lapinmaa'
       email      'tlap@spu.fi'
     end
+
     factory :xerxes do
       association :university, :factory => :akhaimenidien_yliopisto
       #_id        '4f7963bf91bc2bc1f6000006'
@@ -67,6 +72,7 @@ FactoryGirl.define do
       role       :role_union_employee
     end
   end
+
   factory :helsinki_uni_user, class: User do
     email      'halloped@localhost.fi' 
 
@@ -80,6 +86,7 @@ FactoryGirl.define do
       principal_name 'anna@helsinki.fi'
       role       :role_student
     end
+
     factory :helsinki_uni_student_tiina do
       association :university, :factory => :helsinki_uni
       #_id        '4f7963bf91bc2bc1f6000074'
@@ -90,6 +97,7 @@ FactoryGirl.define do
       principal_name 'tiina@helsinki.fi'
       role       :role_student
     end
+
     factory :helsinki_uni_student_pekka do
       association :university, :factory => :helsinki_uni
       #_id        '4f7963bf91bc2bc1f6000078'
@@ -100,6 +108,7 @@ FactoryGirl.define do
       principal_name 'pekka@helsinki.fi'
       role       :role_student
     end
+
     factory :helsinki_uni_student_with_no_phone do
       association :university, :factory => :helsinki_uni
       #_id        '4f7963bf91bc2bc1f6000075'
@@ -110,6 +119,7 @@ FactoryGirl.define do
       principal_name 'nophone@helsinki.fi'
       role       :role_student
     end
+
     factory :helsinki_uni_student_union_employee do
       association :university, :factory => :helsinki_uni
       #_id        '4f7963bf91bc2bc1f6000073'
@@ -121,4 +131,5 @@ FactoryGirl.define do
       role       :role_union_employee
     end
   end
+
 end

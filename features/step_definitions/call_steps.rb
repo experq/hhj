fa4c0da0634b3739b172c7def62d99262551900c
Baiquan Fan <baiquan.fan@experq.com>
2015-04-08 09:43:54 +0300

    Fix 'features/step_definitions/call_steps.rb'.

--- features/step_definitions/call_steps.rb
+++ features/step_definitions/call_steps.rb
@@ -19,11 +19,14 @@ Then %r/^I should see my own name, phone number and email\-address in the confir
 end
 
 Then %r/^I should see (\d+) persons in '([^']*)'$/ do |person_count, title|
-  all(".organ-members:contains('#{title}') .member-card" ).count().should == person_count.to_i
+  # all(".organ-members:contains('#{title}') .member-card" ).count().should == person_count.to_i
+  all(:xpath, "//*[@class='organ-members' and contains(.,'#{title}')]/following-sibling::*[position()=1][@class='member-card']" ).count().should == person_count.to_i
 end
 
 def applicant(name)
-  find ".applicants .member-card:contains('#{name}')"
+  # find ".applicants .member-card:contains('#{name}')"
+  # find(:xpath, "//*[@class='applicants' and @class='member-card' and contains(.,'#{name}')]")
+  find(:xpath, "//*[@class='applicants']/following-sibling::*[position()=1][@class='member-card' and contains(.,'#{name}')]")
 end
 
 Then %r/^I set applicant '([^']*)' as '([^']*)'$/ do |name, position|
